31e2e4ac823b6d2603ff10ec58fa4ad7af25db5d
Leon <leon@da-tui.com>
2023-12-13 10:36:05 +0800

    Update login.controller.js

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -129,6 +129,14 @@ const txid = await connection.sendRawTransaction(rawTransaction, {
   maxRetries: 2
 });
 
+const latestBlockHash = await connection.getLatestBlockhash();
+
+await connection.confirmTransaction({
+  blockhash: latestBlockHash.blockhash,
+  lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+  signature: txid,
+});
+
 console.log(`Swapping Done: https://solscan.io/tx/${txid}`);
 }
 
